4e33a59397b8e82c8bf103a3cf0fc56fd6d2a8c4
JosAlc67 <pjalciva@espol.edu.ec>
2026-08-23 18:00:23 -0500

    fix: Center panel in reachable workspace and correct robot pedestal alignment after track flip

--- ros2_ws/src/irb2600_coating_cell/irb2600_coating_cell/replanning_executor_node.py
+++ ros2_ws/src/irb2600_coating_cell/irb2600_coating_cell/replanning_executor_node.py
@@ -33,7 +33,7 @@ class CoveragePathExecutorNode(StoppableActionNode, Node):
 
         # Target point configuration (will fallback to this if no sensor reading)
         self.declare_parameter("target_structure.frame_id", "world")
-        self.declare_parameter("target_structure.position", [0.6, -2.05, 1.0])
+        self.declare_parameter("target_structure.position", [0.6, -0.55, 1.0])
         self.declare_parameter("target_structure.mesh_file", "package://irb2600_coating_cell/meshes/curved_panel.stl")
         self.declare_parameter("d_standoff", 0.3)
         

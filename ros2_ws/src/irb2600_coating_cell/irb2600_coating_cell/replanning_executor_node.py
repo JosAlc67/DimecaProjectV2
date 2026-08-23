cefca5a7ae342ed11b13564a46dc9e7c25b73d7a
JosAlc67 <pjalciva@espol.edu.ec>
2026-08-23 17:20:06 -0500

    feat: Rotate track 180 degrees to shield cables behind the robot, while perfectly preserving the robot's world-frame facing and workspace reachability towards the workpiece

--- ros2_ws/src/irb2600_coating_cell/irb2600_coating_cell/replanning_executor_node.py
+++ ros2_ws/src/irb2600_coating_cell/irb2600_coating_cell/replanning_executor_node.py
@@ -33,7 +33,7 @@ class CoveragePathExecutorNode(StoppableActionNode, Node):
 
         # Target point configuration (will fallback to this if no sensor reading)
         self.declare_parameter("target_structure.frame_id", "world")
-        self.declare_parameter("target_structure.position", [2.4, -2.25, 1.0])
+        self.declare_parameter("target_structure.position", [1.1, -4.85, 1.0])
         self.declare_parameter("target_structure.mesh_file", "package://irb2600_coating_cell/meshes/tank.stl")
         self.declare_parameter("d_standoff", 0.15)
         
